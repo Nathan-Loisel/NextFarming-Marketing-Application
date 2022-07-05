208cef9691798a87175cdc2efff869a8fb9bd898
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 08:39:39 +0200

    New order improvement

--- public/js/dashboard/orders.js
+++ public/js/dashboard/orders.js
@@ -1,8 +1,20 @@
 var NewOrderSelectedProducts = null;
 var CurrentProducts = null;
 
+// NewOrderSelectedProducts = {
+//    "product id": {
+//        "product": {}
+//        "amount": 0,
+//        "options id": []
+//    }
 
 $(document).ready(function() {
+    $('.ui.modal').modal({
+        allowMultiple: true
+    });
+
+    $('.popupButton').popup();
+
     $('#NewOrderModal').modal({
         closable: false
     });
@@ -15,6 +27,7 @@ $(document).ready(function() {
         $('#NewOrderModal').modal('show');
     }
     );
+
 }
 );
 
@@ -22,33 +35,6 @@ function LoadProducts(Search, Page){
     if(Page == undefined) {
         Page = 1;
     }
-
-    // Example:
-    // var data = {
-    //     Amount: 10,
-    //     Page: 1
-    // }
-    // api.post('/product/list', data)
-    // .then(res => {
-    //     $('#ProductsTable').html('');
-    //     res.data.message.forEach(function(Product) {
-    //         AddProductToTable(Product);
-    //     });
-    // }
-    // )
-    // .catch(error => {    
-    //     console.log(error);
-    //     if(error.response != null && error.response.data != null){
-    //         if(error.response.data.message == undefined){
-    //             ShowNotif("Server Error", 'red');
-    //         };
-    //         ShowNotif(error.response.data.message, 'red');
-    //     }
-    //     else{
-    //         ShowNotif("Server Error", 'red');
-    //     }
-    // }
-    // );
 
     var data = {
         Amount: 10,
@@ -66,7 +52,6 @@ function LoadProducts(Search, Page){
     }
     )
     .catch(error => {
-        console.log(error);
         if(error.response != null && error.response.data != null){
             if(error.response.data.message == undefined){
                 ShowNotif("Server Error", 'red');
@@ -83,18 +68,42 @@ function LoadProducts(Search, Page){
 
 function AddProductInList(Product){
     $('#ProductsList').append(
-        '<div class="item">' +
-        '<div class="right floated content">' +
-        '<div class="ui green button" onclick="AddProductToOrder(' + Product.ID + ')">Add</div>' +
-        '<div class="ui red button" onclick="DeleteProductFromOrder(' + Product.ID + ')">Delete</div>' +
-        '<div class="ui input">' +
-        '<input type="number" name="product-' + Product.ID + '" id="product-' + Product.ID + '" value="0" min="0" max="100">' +
-        '</div>' +
-        '</div>' +
-        '<img class="ui avatar image" src="/images/avatar2/small/lena.png">' +
-        '<div class="content">' +
-        Product.Title +
-        '</div>' +
-        '</div>'
+        `<div class="item">
+            <div class="right floated content">
+                <button class="ui icon compact blue button popupButton" data-content="Add options" onclick="OpenNewOrderOptionsModal('${Product.ID}')">
+                    <i class="box icon"></i>
+                </button>
+                <button class="ui icon compact green button popupButton"  data-content="Add product" onclick="AddProductToNewOrder('${Product.ID}')">
+                    <i class="plus icon"></i>
+                </button>
+                <button class="ui icon compact button popupButton" data-content="Reset product">
+                    <i class="redo icon"></i>
+                </button>
+            </div>
+            <img class="ui avatar image" src="/">
+            <div class="content">
+                <span style="margin-right: 10px;" class="popupButton" data-content="${Product.ShortDescription}d">${Product.Title}</span>
+                <div class="ui label">
+                    ${Product.Price}€
+                </div>
+            </div>
+        </div>`
     );
+    $('.popupButton').popup();
+}
+
+function AddProductToNewOrder(ProductID){
+    if(NewOrderSelectedProducts == null){
+        NewOrderSelectedProducts = {};
+    }
+
+    if(NewOrderSelectedProducts[ProductID] != undefined){
+        SelectedOptions = CurrentProducts.find(function(Product) { return Product.ID == ProductID; }).SelectedOptions;
+
+        
+    }
+}
+
+function OpenNewOrderOptionsModal(ProductID){
+    $('#NewOrderOptionsModal').modal('show');
 }
